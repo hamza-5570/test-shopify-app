c80fa827b15da0a996bbc29bd999cfc89067ab6e
hamza-5570 <hafizameerhamza87@gmail.com>
2025-04-21 21:22:40 +0500

    changes

--- remix.config.js
+++ remix.config.js
@@ -15,8 +15,7 @@ module.exports = {
   ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
   serverModuleFormat: "cjs",
-  server: './server.js',
-  serverBuildTarget: 'vercel-serverless',
+  serverBuildTarget: "vercel",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
   future: {},
 };
