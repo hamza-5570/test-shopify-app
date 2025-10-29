2bd6189c6835230437e0e2dea220633e603c6579
hamza-5570 <hafizameerhamza87@gmail.com>
2025-10-29 16:01:45 +0500

    changes

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,12 +1,12 @@
 import { Page, Layout, Text, Card, Button } from "@shopify/polaris";
 import { authenticate } from "../shopify.server";
 import { Redirect } from "@shopify/app-bridge/actions";
-import { createApp} from "@shopify/app-bridge";
+import { createApp } from "@shopify/app-bridge";
 import { useAppBridge } from "@shopify/app-bridge-react";
-import {useLoaderData} from "@remix-run/react"
+import { useLoaderData } from "@remix-run/react";
 export const loader = async ({ request }) => {
- const {admin}= await authenticate.admin(request);
- const response=await admin.graphql(`
+  const { admin } = await authenticate.admin(request);
+  const response = await admin.graphql(`
     query {
       shop {
         name
@@ -17,21 +17,23 @@ export const loader = async ({ request }) => {
     }
   `);
 
- const shop =  await response.json();
+  const shop = await response.json();
   return shop.data.shop;
 };
 
 export default function Index() {
-  const shop=useLoaderData();
+  const shop = useLoaderData();
   const app = createApp({
     apiKey: "44c605e398e252fbc7fdd0cc5a74d230",
-    host: new URLSearchParams(typeof window !== "undefined" && window.location.search).get("host"),
+    host: new URLSearchParams(
+      typeof window !== "undefined" && window.location.search,
+    ).get("host"),
   });
-  const handleRedirect =async () => {
-    const redirect = Redirect.create(app);    
+  const handleRedirect = async () => {
+    const redirect = Redirect.create(app);
     redirect.dispatch(
       Redirect.Action.REMOTE,
-      `https://wonport.vercel.app/firstAuthFromShopify?shop=${shop.myshopifyDomain}&email=${shop?.email}&appUrl=${shop.url}`
+      `https://wonport-version-2.vercel.app/firstAuthFromShopify?shop=${shop.myshopifyDomain}&email=${shop?.email}&appUrl=${shop.url}`,
     );
   };
 
@@ -43,10 +45,12 @@ export default function Index() {
             <Text variant="headingLg" as="h2" alignment="center">
               Connect to Wonport App
             </Text>
-            <center style={{
-              marginTop:"10px"
-            }}>
-            <Button  onClick={handleRedirect}>Connect</Button>
+            <center
+              style={{
+                marginTop: "10px",
+              }}
+            >
+              <Button onClick={handleRedirect}>Connect</Button>
             </center>
           </Card>
         </Layout.Section>
